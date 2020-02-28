57cf3d9db1423af1d25d3641100af53c1df1732f
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-02-28 10:34:03 -0300

    Calculadora Implementando

--- assets/js/calculatorBrain.js
+++ assets/js/calculatorBrain.js
@@ -1,4 +1,7 @@
+// Variaveis Globais
 var result = document.getElementById("result");
+var n1, n2; //Declaração de variaveis
+var operacao="";
 
 function onNumberClicked(numberButton){
     
@@ -12,27 +15,70 @@ function onNumberClicked(numberButton){
 
 function onClearClicked(){
     result.innerHTML = "0";
+    n1=0;
+    n2=0;
 }
 
 function onEqualsClicked(){
     //TODO implement this function
+    n2=parseInt(result.innerHTML); //Atribuição e conversão para numero
+    if(operacao=="+")
+    {
+        result.innerHTML = n1+n2;
+    }
+    if(operacao=="-")
+    {
+        result.innerHTML = n1-n2;
+    }
+    if(operacao=="*")
+    {
+        result.innerHTML = n1*n2;
+    }
+    if(operacao=="/")
+    {
+        if (n2>0){
+            result.innerHTML = n1/n2;
+        }
+        else {
+            result.innerHTML = "Impossivel Dividir por zero!"
+        }
+        
+    }   
+
 }
 
 function onDivideClicked(){
     //TODO implement this function
+    n1 = result.innerHTML
+    n1 = parseInt(n1); //Convertendo para numero
+    operacao = "/"
+    result.innerHTML = "0";
 }
 
 
 
 function onAddClicked(){
     //TODO implement this function
+    n1 = result.innerHTML
+    n1 = parseInt(n1); //Convertendo para numero
+    operacao = "+"
+    result.innerHTML = "0";
+   
+
 }
 
 function onSubtractClicked(){
     //TODO implement this function
+    n1 = result.innerHTML
+    n1 = parseInt(n1); //Convertendo para numero
+    operacao = "-"
+    result.innerHTML = "0";
 }
 
 function onMultiplyCliked(){
-    //TODO implement this function
+    n1 = result.innerHTML
+    n1 = parseInt(n1); //Convertendo para numero
+    operacao = "*"
+    result.innerHTML = "0";
 }
 
